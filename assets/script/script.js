b371a4361e7db43db979e705124cf8f8a80ca271
gderuwez <gderuwez@gmail.com>
2018-06-19 10:59:29 +0200

    home button created

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,21 +1,21 @@
 // Pop-Up age
 
 $(document).ready(function(){
-  var result = false;
-  var age = "";
-  function test() {
-    var name = prompt("Indiquez votre âge : ");
-    var entree = parseInt (name, 10);
-    result = Number.isInteger(entree)
-    age = entree;
-  }
-
-  while(result == false) {
-    test();
-  }
-  if (age < 18) {
-    window.location.href = 'https://www.imdb.com/';
-  }
+  // var result = false;
+  // var age = "";
+  // function test() {
+  //   var name = prompt("Indiquez votre âge : ");
+  //   var entree = parseInt (name, 10);
+  //   result = Number.isInteger(entree)
+  //   age = entree;
+  // }
+  //
+  // while(result == false) {
+  //   test();
+  // }
+  // if (age < 18) {
+  //   window.location.href = 'https://www.imdb.com/';
+  // }
 
   let hidding = (select) => {
     $(select).hide();
@@ -45,4 +45,18 @@ $(document).ready(function(){
   $(".vanish2").click(function(){
     hidding("#popup-register");
   });
+
+  var newButton = () =>{
+    var homeButton = document.createElement("a");
+    var newText = document.createTextNode("test");
+    homeButton.appendChild(newText);
+    homeButton.setAttribute("class", "btn btn-primary text-center")
+    homeButton.setAttribute("href", "#top")
+    var currentFooter = document.getElementById('footer');
+    currentFooter.insertAdjacentElement("beforeend", homeButton);
+    console.log("test");
+  };
+
+  newButton();
+
 });
